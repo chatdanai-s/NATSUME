22d7e74f82d98b275375c76fbfe4c7eeff997027
Chatdanai Sawangwong <chatdanai.saw@gmail.com>
2025-08-20 10:32:15 +0700

    Added first files

---
+++ natsume/DeckAgol.py
@@ -0,0 +1,7 @@
+# Functions that only exist in the Deck-Agol (2016) literature
+# Inversion functions
+def DeckAgolOuterInversion():
+    pass
+
+def DeckAgolInnerInversion():
+    pass
---
+++ natsume/common.py
@@ -0,0 +1,27 @@
+# Functions shared by Lithwick (2012) and Deck & Agol (2016) literatures
+import numpy as np
+from scipy.integrate import quad
+
+# Numerical differentation of f(x) with O(eps^2) error
+def numericDiff():
+    pass
+
+# Determine MMR for string
+def MMR(MMRstr):
+    pass
+    
+# Semi-major axis ratio
+def alpha():
+    pass
+
+def b():
+    pass
+
+def InnerPeriods():
+    pass
+
+def OuterPeriods():
+    pass
+
+def NormalizedResonanceDistance():
+    pass
