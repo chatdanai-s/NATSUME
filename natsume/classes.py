Fix Lithwick not converting angle to radians

natsume/classes.py
@@ -2,7 +2,7 @@ import numpy as np
 
 ### Classes
 # Complex eccentricities containing the eccentricity and
-# longitude of periastron for inner and outer planets
+# longitude of periastron for inner and outer planets (input in deg, kept as rad!) 
 class ComplexEccentricities:
     def __init__(self,
                  inner_e=0, inner_periastron=0,
@@ -11,9 +11,9 @@ class ComplexEccentricities:
             if (inner_e < 0) or (outer_e < 0):
                 raise ValueError(f'inner_e and outer_e in ComplexEccentricities must be greater than or equal to 0.')
             self.inner_e = float(inner_e)
-            self.inner_periastron = float(inner_periastron)
+            self.inner_periastron = np.deg2rad(float(inner_periastron))
             self.outer_e = float(outer_e)
-            self.outer_periastron = float(outer_periastron)
+            self.outer_periastron = np.deg2rad(float(outer_periastron))
 
         except (TypeError, ValueError):
             raise TypeError(f'All ComplexEccentricities arguments must be a float or integer.')