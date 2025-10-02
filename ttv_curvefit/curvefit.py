89e6d8847afde21178091bc76bc5e99c01f8af32
Chatdanai Sawangwong <chatdanai.saw@gmail.com>
2025-10-02 15:05:04 +0700

    Add lmfit option for TTV curve fit

--- ttv_curvefit/curvefit.py
+++ ttv_curvefit/curvefit.py
@@ -3,6 +3,7 @@
 # Lomb-Scargle periodogram not used because superperiod can already be estimated from TTVFast inputs
 import numpy as np
 from scipy.optimize import curve_fit
+import lmfit
 
 def return_ttv(epochs, midtransits):
     # TTVFast output
@@ -33,21 +34,40 @@ def return_ttv(epochs, midtransits):
 def ttv_model(t, A, B, V, Pttv, phase):
     return A + B*t + V * np.sin(2*np.pi/Pttv * t + phase)
     
-def characterize_ttv(time, ttv, ttv_err, expected_period):
+def characterize_ttv(time, ttv, ttv_err, expected_period, method='scipy'):
     # Fit sine curve to TTV
     V_guess = (ttv.max() - ttv.min()) / 2
     Pttv_guess = expected_period
     phase_guess = (np.pi/2 - 2*np.pi/Pttv_guess * time[np.argmax(ttv)]) % (2 * np.pi)
 
     p0 = [0, 0, V_guess, Pttv_guess, phase_guess]  # Initial guess for parameters
-    popt_ttv, pcov_ttv = curve_fit(f=ttv_model, xdata=time, ydata=ttv, p0=p0,
-                                   bounds=([-np.inf, -np.inf, 0, 0, -np.pi],
-                                           [np.inf, np.inf, 2*V_guess, np.inf, 3*np.pi]))
-    perr_ttv = np.sqrt(np.diag(pcov_ttv))
 
-    # Acquire reduced chi-squares
-    residuals = ttv - ttv_model(time, *popt_ttv)
-    red_chi2 = np.sum((residuals/ttv_err)**2) / (len(ttv) - len(popt_ttv))
+    if method == 'scipy':
+        popt_ttv, pcov_ttv = curve_fit(f=ttv_model, xdata=time, ydata=ttv, p0=p0,
+                                    bounds=([-np.inf, -np.inf, 0, 0, -np.pi],
+                                            [np.inf, np.inf, 2*V_guess, np.inf, 3*np.pi]))
+        perr_ttv = np.sqrt(np.diag(pcov_ttv))
+
+        # Predictions
+        ttv_pred = ttv_model(time, *popt_ttv)
+
+        # R² manually
+        ss_res = np.sum((ttv - ttv_pred)**2)
+        ss_tot = np.sum((ttv - np.mean(ttv))**2)
+        r2 = 1 - (ss_res / ss_tot)
+
+    if method == 'lmfit':
+        model = lmfit.Model(ttv_model)
+        
+        # Fitting part
+        result = model.fit(ttv, t=time, A=0, B=0, V=V_guess, Pttv=Pttv_guess, phase=phase_guess)
+
+        # Extract values and uncertainties
+        popt_ttv = [result.params[p].value for p in ['A', 'B', 'V', 'Pttv', 'phase']]
+        perr_ttv = [result.params[p].stderr for p in ['A', 'B', 'V', 'Pttv', 'phase']]
+
+        # Reduced chi-square
+        r2 = result.rsquared
 
     # Return [A, B, V, Pttv, phase], their errors, and red-chi2 score
-    return popt_ttv, perr_ttv, red_chi2
+    return popt_ttv, perr_ttv, r2
