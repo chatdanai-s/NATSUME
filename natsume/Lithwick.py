d40250e3909849afcda15f460117e367f048b9dd
Chatdanai Sawangwong <chatdanai.saw@gmail.com>
2025-12-22 21:21:37 +0700

    Documentation in .classes and .Lithwick

--- natsume/Lithwick.py
+++ natsume/Lithwick.py
@@ -1,17 +1,38 @@
 # Functions that only exist in the Lithwick (2012) literature
 import numpy as np
-
 from .classes import ComplexEccentricities, TTVSineCurve
 from .common import get_b, get_Db, get_alpha, get_NormalizedResonanceDistance, get_outerPeriods, get_innerPeriods
 
 # Lithwick disturbing functions
 def get_f(alpha, j: int):
+    """
+    Returns sum of Laplace coefficients with order-unity values (denoted f),
+    as described in Table 3 of Lithwick et al. (2012).
+
+    Args:
+        alpha: Ratio of orbit semi-major axes, see natsume.common.get_alpha (float or array)
+        j: Number denoting j:j-1 mean motion resonance of system (integer)
+
+    Returns:
+        f(alpha) as described in Table 3 of Lithwick et al. (2012). (float or array)
+    """
     return -(j * get_b(alpha, j)) - (alpha/2 * get_Db(alpha, j, order=1))
 
 def get_g(alpha, j: int):
+    """
+    Returns sum of Laplace coefficients with order-unity values (denoted g),
+    as described in Table 3 of Lithwick et al. (2012).
+
+    Args:
+        alpha: Ratio of orbit semi-major axes, see natsume.common.get_alpha (float or array)
+        j: Number denoting j:j-1 mean motion resonance of system (integer)
+
+    Returns:
+        g(alpha) as described in Table 3 of Lithwick et al. (2012). (float or array)
+    """
     if j == 2:
         # -1/(2 * alpha^2) s.t. alpha ~ (1/2)**(2/3) for inner perturber,
-        # -2 * alpha for outer perturber, which are identical!
+        # -2 * alpha for outer perturber, which are identical.
         correction = -1.25992104989
     else:
         correction = 0
@@ -19,6 +40,17 @@ def get_g(alpha, j: int):
 
 # Weighted average of free eccentricities
 def get_Zfree(f, g, z: ComplexEccentricities):
+    """
+    Returns weighted average of free eccentricities as described in equation 10 of Lithwick et al. (2012).
+
+    Args:
+        f: Sum of Laplace coefficients with order-unity values, see natsume.Lithwick.get_f (float or array)
+        g: Sum of Laplace coefficients with order-unity values, see natsume.Lithwick.get_g (float or array)
+        z: Complex eccentricities of two planets in the system (class ComplexEccentricies)
+
+    Returns:
+        Z_free as described in equation 10 of Lithwick et al. (2012). (float or array)
+    """
     return (f * z.inner_e * np.exp(1j * z.inner_periastron)) + \
            (g * z.outer_e * np.exp(1j * z.outer_periastron))
 
@@ -26,6 +58,22 @@ def get_Zfree(f, g, z: ComplexEccentricities):
 # Inversion functions
 def LithwickOuterInversion(innerTTV: TTVSineCurve, innerPeriod: float,
                            j: int, z: ComplexEccentricities, outerPeriod='none'):
+    """
+    Returns mass of outer planet analytically computed from inner TTV sine curve,
+    as described in equation 8 of Lithwick et al. (2012).
+
+    Args:
+        innerTTV: Inner TTV amplitude and TTV period, in days (class TTVSineCurve)
+        innerPeriod: Orbital period of inner planet, in days (float)
+        j: Number denoting j:j-1 mean motion resonance of system (integer)
+        z: Complex eccentricities of two planets in the system (class ComplexEccentricies)
+        outerPeriod: Period of outer planet, in days, if known ('none' or float)
+
+    Returns:
+        mu: Mass of outer planet with unit of host star mass (array or float).
+        If outerPeriod == 'none', mu will be an array with two possible mass solutions.
+        If outerPeriod is given, mu will be a float with one possible mass solution.
+    """
     if outerPeriod == 'none':
         outerPeriods = get_outerPeriods(innerPeriod, innerTTV.superperiod, j, N=1)
     else:
@@ -43,6 +91,22 @@ def LithwickOuterInversion(innerTTV: TTVSineCurve, innerPeriod: float,
 
 def LithwickInnerInversion(outerTTV: TTVSineCurve, outerPeriod: float,
                            j: int, z: ComplexEccentricities, innerPeriod='none'):
+    """
+    Returns mass of inner planet analytically computed from outer TTV sine curve,
+    as described in equation 9 of Lithwick et al. (2012).
+
+    Args:
+        outerTTV: Outer TTV amplitude and TTV period, in days (class TTVSineCurve)
+        outerPeriod: Orbital period of outer planet, in days (float)
+        j: Number denoting j:j-1 mean motion resonance of system (integer)
+        z: Complex eccentricities of two planets in the system (class ComplexEccentricies)
+        innerPeriod: Period of inner planet, in days, if known ('none' or float)
+
+    Returns:
+        mu: Mass of inner planet with unit of host star mass (array or float).
+        If innerPeriod == 'none', mu will be an array with two possible mass solutions.
+        If innerPeriod is given, mu will be a float with one possible mass solution.
+    """
     if innerPeriod == 'none':
         innerPeriods = get_innerPeriods(outerPeriod, outerTTV.superperiod, j, N=1)
     else:
--- natsume/classes.py
+++ natsume/classes.py
@@ -7,6 +7,19 @@ class ComplexEccentricities:
     def __init__(self,
                  inner_e=0, inner_periastron=0,
                  outer_e=0, outer_periastron=0):
+        """
+        Returns complex eccentricities (eccentricity and longitude of periastron) of two planets in the system.
+        Used as input in functions requiring eccentricies and longitudes of periastron.
+
+        Args:
+            inner_e: Inner orbital eccentricity, must be at least 0 (float)
+            inner_periastron: Inner longitude of periastron, in degrees (float)
+            outer_e: Outer orbital eccentricity, must be at least 0 (float)
+            outer_periastron: Outer longitude of periastron, in degrees (float)
+
+        Returns:
+            Complex eccentricities of two planets in the system (class ComplexEccentricities)
+        """
         try:
             if (inner_e < 0) or (outer_e < 0):
                 raise ValueError(f'inner_e and outer_e in ComplexEccentricities must be greater than or equal to 0.')
@@ -20,12 +33,33 @@ class ComplexEccentricities:
 
     @property
     def arr(self):
+        """
+        Returns array representation of values in class ComplexEccentricties object.
+
+        Args:
+            None
+
+        Returns:
+            Array object containing inner eccentricity, inner periastron (rad),
+            outer eccentricity, and outer periastron (rad), in order
+        """
         return np.array([self.inner_e, self.inner_periastron,
                          self.outer_e, self.outer_periastron])
 
 # TTV Sine curve with amplitude in minutes and superperiod in days
 class TTVSineCurve:
     def __init__(self, amplitude: float, superperiod: float):
+        """
+        Returns TTV sine curve representation (Amplitude and Superperiod) of a planet, both in days.
+        Used as input in functions requiring sinusoidal TTV curves.
+
+        Args:
+            amplitude: TTV amplitude in days, must be at least 0 (float)
+            superperiod: TTV superperiod in days, must be at least 0 (float)
+
+        Returns:
+            TTV sine curve representation of a planet (class TTVSineCurve)
+        """
         try:
             if (amplitude <= 0) or (superperiod <= 0):
                 raise ValueError(f'Amplitude and superperiod in TTVSineCurve must be greater than 0.')
@@ -37,4 +71,13 @@ class TTVSineCurve:
         
     @property
     def arr(self):
+        """
+        Returns array representation of values in class TTVSineCurve object.
+
+        Args:
+            None
+
+        Returns:
+            Array object containing TTV amplitude and TTV superperiod, in order
+        """
         return np.array([self.amplitude, self.superperiod])
